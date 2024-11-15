b6db05946c967af54236dbbe54b821d7edd6ecff
Flavia Voicu <flavia.voicuu@gmail.com>
2024-11-15 14:58:51 +0100

    Return np.array for normalized state and fix step return

--- pong_ai/env.py
+++ pong_ai/env.py
@@ -1,6 +1,7 @@
 import json
 import time
 import os
+import numpy as np
 
 STATE_FILE_PATH = "./game_state.json"
 LOG_FILE_PATH = "./data/game_log.json"
@@ -29,7 +30,7 @@ class PongEnv:
             return json.load(f)
         # Default state if the file doesn t exist
         return {
-            "ai_paddle": self.screen_height / 2 - self.paddle_height / 2,
+            "ai_paddle": (self.screen_height / 2 - self.paddle_height / 2),
             "ball_x": 400,
             "ball_y": 200,
             "ball_speed_x": 5,
@@ -42,6 +43,7 @@ class PongEnv:
         self.state = self.load_state()
         return self.get_normalized_state()
 
+    # Step returns (state, reward, done) tuple, with state as np.array
     def step(self, action):
         # Action = continuous value ==> paddle velocity
         # Clip the action to the maximum paddle speed
@@ -62,17 +64,17 @@ class PongEnv:
         self.log_state(action, reward)
         self.save_state()
 
-        return self.get_normalized_state, reward, self.done
+        return self.get_normalized_state(), reward, self.done
 
     def get_normalized_state(self):
-        normalized_state = {
+        normalized_state = np.array([
             "ai_paddle": (self.state["ai_paddle"] / (self.screen_height - self.paddle_height)) * 2 - 1,
             "ball_x": (self.state["ball_x"] / self.screen_height) * 2 - 1,
             "ball_y": (self.state["ball_y"] / self.screen_height) * 2 - 1,
             "ball_speed_x": self.state["ball_speed_x"] / self.max_paddle_speed,
             "ball_speed_y": self.state["ball_speed_y"] / self.max_paddle_speed,
             "ball_missed": self.state["ball_missed"]
-        }
+        ], dtype=np.float32)
         return normalized_state
 
     def update_ball(self):
