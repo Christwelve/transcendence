74d149769bdcad3b6e665040e31f809c00a737dc
Flavia Voicu <flavia.voicuu@gmail.com>
2024-11-15 17:05:59 +0100

    Initialize PPOAgent class with hyperparameter configuration for actor-critic networks

--- pong_ai/ppo_agent.py
+++ pong_ai/ppo_agent.py
@@ -4,7 +4,18 @@ from tensorflow.keras import layers, optimizers, losses
 
 class PPOAgent:
     # Init the PPO agent with given hyperparams
-    def __init__(self):
+    def __init__(self,
+                 input_dim, # dim of the input(state space)
+                 action_dim, # dim of the output
+                 actor_lr = 0.003, # actor nw --> predicts policy based on state
+                 critic_lr=0.001, # critic nw --> predicts value of state
+                 gamma=0.99, # discound factor for future rewards
+                 epsilon=0.2, # clipping parameter, limits the update size of the policy
+                 lambda_gae=0.95, # balances bias and variance in advantage estimation
+                 c1=0.5, # entropy coeff in ppo obj, balances contribution of value loss relative to policy loss
+                 c2=0.01 # entropy coeff in ppo obj, encourages exploration
+                 
+                 ):
         pass
 
     # Build the policy and value models
