722a7fe15f038c4539b145c1d0085dc4c94cb037
Christina-Repou <chr.repou@gmail.com>
2024-11-23 19:31:38 +0100

    create a new endpoints to login and get a user by username, implement login functionality

--- django/api/views.py
+++ django/api/views.py
@@ -3,16 +3,37 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import User, Match, Statistic
 from .serializers import UserSerializer, MatchSerializer, StatisticSerializer
+from django.contrib.auth.hashers import make_password, check_password
+from django.shortcuts import get_object_or_404
+
+@api_view(['POST'])
+def login_view(request):
+    if request.method == 'POST':
+        username = request.data['username']
+        password = request.data['password']
+        user = get_object_or_404(User, username=username)
+        if check_password(password, user.password):
+            serializer = UserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'POST'])
-def user_view(request):
+def user_view(request, username=None):
     if request.method == 'GET':
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True, context={'request': request})
-        return Response(serializer.data)
+        if username:
+            user = get_object_or_404(User, username=username)
+            serializer = UserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            users = User.objects.all()
+            print(request)
+            serializer = UserSerializer(users, many=True, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
+        userData = request.data.copy()
+        userData['password'] = make_password(userData['password'])
+        serializer = UserSerializer(data=userData)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
