50ce1e7fda74e67276f4b727bd41270e354bac5e
dendeaisd <flavia.voicuu@gmail.com>
2024-11-29 05:05:27 +0200

    Friend model and admin  interface are set up and functional

--- django/api/admin.py
+++ django/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Friend
+
 
 # Register your models here.
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    list_display = ('user', 'friend', 'created_at')
+    search_fields = ('user__username', 'friend__username')
