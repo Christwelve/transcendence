ac49c5f289d908fb72e7df55ea71345965532382
gero-boehm <gero_boehm@gmx.de>
2024-12-22 22:37:33 +0100

    forgetti spaghetti

--- react/src/App.js
+++ react/src/App.js
@@ -129,13 +129,13 @@ function App() {
     }
   };
 
-  return (
-    <DataContextProvider>
-      <Home changeStatus={changeStatus} avatar={avatar} />
-      <ModalPresenter />
-      <ToastPresenter />
-    </DataContextProvider>
-  );
+  // return (
+  //   <DataContextProvider>
+  //     <Home changeStatus={changeStatus} avatar={avatar} />
+  //     <ModalPresenter />
+  //     <ToastPresenter />
+  //   </DataContextProvider>
+  // );
 
   return (
     <>
