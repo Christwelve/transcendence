64b177ea534677ea81ac2d0921cdd2c27ce23dc8
adinari42 <anasdinari@gmail.com>
2024-12-27 09:58:11 +0100

    added urls for the new views endpoints

--- django/api/urls.py
+++ django/api/urls.py
@@ -3,9 +3,11 @@ from oauth2_provider import urls as oauth2_urls
 from . import views
 
 urlpatterns = [
+    path('fetchUsers/', views.get_all_users, name='get_all_users'),
     path('users/', views.user_view, name='user_view'),
 	path('users/<str:username>/', views.user_view, name='user_view_by_username'),
-	path('user/data/', views.get_user_data, name='user_data'),
+	path('testSession', views.test_session, name='test_session'),
+    path('user/data/', views.get_user_data, name='user_data'),
     path('matches/', views.match_view, name='match_view'),
     path('statistics/', views.statistic_view, name='statistic_view'),
     path('login/', views.login_view, name='login_view'),
