45440b1cc80cdaa95832c40c3282d3a3fc3697bc
Anas Dinari <anasdinari@gmail.com>
2025-01-02 14:02:18 +0100

    added statistics support for lobby

--- react/src/components/Lobby/Lobby.js
+++ react/src/components/Lobby/Lobby.js
@@ -1,21 +1,54 @@
-import React, {useState} from 'react'
+import React, {useState, useMemo} from 'react'
 import RoomList from '../RoomList/RoomList'
 import Room from '../Room/Room'
 import Friends from '../Friends/Friends'
+import Statistics from '../Statistics/Statistics'
 import scss from './Lobby.module.scss'
+import { showToast } from '../Toast/ToastPresenter'
+import { useDataContext } from '../../context/DataContext'
 
 function Lobby() {
-  return (
-    <div className={scss.lobby}>
-      <div className={scss.top}>
-        <RoomList />
-        <Room />
-      </div>
-      <div className={scss.bottom}>
-  			<Friends />
-      </div>
-    </div>
-  )
+	const [selectedFriend, setSelectedFriend] = useState(null);
+	const [playerStats, setPlayerStats] = useState([]);
+	const [friendStats, setFriendStats] = useState([]);
+
+	const {getPlayer} = useDataContext();
+
+	const player = getPlayer();
+
+	useMemo(() => getStatistics(player.tid, setPlayerStats), [player.tid]);
+	useMemo(() => getStatistics(selectedFriend, setFriendStats), [selectedFriend]);
+
+	return (
+		<div className={scss.lobby}>
+			<div className={scss.top}>
+				<RoomList />
+				<Room />
+			</div>
+			<div className={scss.bottom}>
+					<Friends selected={[selectedFriend, setSelectedFriend]} />
+					<Statistics title='Friend' data={friendStats} tid={selectedFriend} />
+					<Statistics title='You' data={playerStats} tid={player.tid} />
+			</div>
+		</div>
+	)
+}
+
+// helper functions
+async function getStatistics(tid, setStats) {
+	if(tid == null)
+		return setStats([]);
+
+	try {
+		const response = await fetch(`http://localhost:8000/api/statistics?userId=${tid}`);
+		const data = await response.json();
+
+		setStats(data);
+	}
+	catch (error) {
+		showToast({type: 'warning', title: 'Warning', message: 'Failed to get statistics.'});
+		setStats([]);
+	}
 }
 
 export default Lobby;
